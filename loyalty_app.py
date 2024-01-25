1eda1babc3343c1ebc94fe2c63b36a2b074780a2
Drazen Dodik <drazend@live.fi>
2024-01-25 09:50:51 +0200

    add routes

--- loyalty_app.py
+++ loyalty_app.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI, File, UploadFile
 import csv
-from helpers import *
 
 app = FastAPI()
+
+@app.middleware("http")
+async def process_valohai_prefix(request: Request, call_next):
+    path = request.scope["path"]
+    for prefix in (
+        request.headers.get("X-VH-Prefix"),
+        os.environ.get("VH_DEFAULT_PREFIX"),
+    ):
+        if not prefix:  # Could have no header or no envvar, so skip
+            continue
+        if path.startswith(prefix):  # If the path starts with this prefix,
+            # ... tell FastAPI (0.56.0+) that that is the prefix we're mounted under...
+            request.scope["root_path"] = prefix
+            # ... then strip the prefix out as far as FastAPI is concerned.
+            request.scope["path"] = "/" + path[len(prefix) :].lstrip("/")
+            break
+    return await call_next(request)
 
 data_path = 'data.csv'
 loaded_data = None
